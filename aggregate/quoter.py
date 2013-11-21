213fd283027ff33d228871002d2c2bd6b99f8287
Chau Tran <chau_tran@brown.edu>
2013-11-20 23:21:39 -0500

    Added some analysis code

---
+++ aggregate/quoter.py
@@ -0,0 +1,9 @@
+f = open('0_1000.csv')
+for line in f:
+    fields = line.split(",")
+    id = fields[0]
+    tags = fields[1].strip()
+    if (id != "Id"):
+        print "%s,\"%s\"" % (id,tags)
+    else:
+        print line
---
+++ preprocess/analyze_tags.py
@@ -0,0 +1,27 @@
+import csv
+import json
+import sys
+import random
+
+csv_file = open(sys.argv[1])
+reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+
+tag_file = open(sys.argv[2])
+sorted_tags = json.load(tag_file)
+
+N = int(sys.argv[3])
+tags = set(map(lambda x: x[0], sorted_tags[:N]))
+data = []
+header = True
+found = 0.
+for row in reader:
+    if (header):
+        header = False
+        continue
+    q_tags = row[3].split()
+    for q_tag in q_tags:
+        if q_tag in tags:
+            found += 1
+            continue
+
+print "Questions with tag found: ",found
