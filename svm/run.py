be34ccd6ece5cbab5353b31632588bed7ed9d7aa
Chau Tran <chau_tran@brown.edu>
2013-12-04 06:32:47 +0000

    mkdir if not exist

--- svm/run.py
+++ svm/run.py
@@ -1,6 +1,12 @@
+import sys
 from subprocess import call
+import os
+from os.path import isdir
+if !isdir("../results/5percent/"+range):
+    call(["mkdir", "../results/5percent/"+range)
+range = sys.argv[1]
 for i in range(0,21):
     input_name = "../data/test/test_"+str(i)+".json"
-    output_name = "../results/5percent/0_100/test_"+str(i)+".csv"
+    output_name = "../results/5percent/"+range+"/test_"+str(i)+".csv"
     call(["python","scikit_svm.py","get_tags",input_name,output_name])
 
