9dd50c8d13a6c97483ee96b515f3d6a537761995
Chau Tran <chau_tran@brown.edu>
2013-12-04 06:53:04 +0000

    Fix bug run.py

--- svm/run.py
+++ svm/run.py
@@ -2,11 +2,12 @@ import sys
 from subprocess import call
 import os
 from os.path import isdir
-if !isdir("../results/5percent/"+range):
-    call(["mkdir", "../results/5percent/"+range)
-range = sys.argv[1]
+
+ra= sys.argv[1]
+if not isdir("../results/5percent/"+ra):
+    call(["mkdir", "../results/5percent/"+ra])
 for i in range(0,21):
     input_name = "../data/test/test_"+str(i)+".json"
-    output_name = "../results/5percent/"+range+"/test_"+str(i)+".csv"
+    output_name = "../results/5percent/"+ra+"/test_"+str(i)+".csv"
     call(["python","scikit_svm.py","get_tags",input_name,output_name])
 
